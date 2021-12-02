604309e0bf2ad9c4118eba9b5ea1dff1584106c2
Tuuli TG <tuuli.k.toivanen@gmail.com>
2021-12-02 22:12:38 +0200

    Tehtävä 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,19 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons'
 import Search from './components/Search'
 import Form from './components/AddPersonForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-    { name: 'Maija Meikäläinen',
-      number: '040-123456'
-    }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNumber] = useState('')
   const [searchBar, setSearchBar] = useState('')
 
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  
   const addNewPersons = () => {
     const hasNumber = newNumber.length > 0
     const nameObject = {
