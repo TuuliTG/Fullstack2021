cf7e5eff32e39a047fe23c41c9f5e525e69e57de
Tuuli TG <tuuli.k.toivanen@gmail.com>
2021-12-30 18:01:49 +0200

    Tehtävä 3.17

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -53,6 +53,22 @@ app.delete('/api/persons/:id', (req, res, next) => {
         .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+    const id = req.params.id
+    console.log('Updating person with id:',id)
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+    console.log('UPDATING', person)
+    Person.findByIdAndUpdate(id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
 app.post('/api/persons', (req, res, next) => {
     const body = req.body
 
