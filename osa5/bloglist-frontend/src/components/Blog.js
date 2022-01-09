4afda567239b91480c0679b0baf8834a0a64914a
Tuuli TG <tuuli.k.toivanen@gmail.com>
2022-01-09 15:19:49 +0200

    Tehtävä 5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,43 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
 
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+  
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  if(visible) {
+    return(
+      <div style={blogStyle}>
+      <p>{blog.title} {blog.author}</p>
+      <p>{blog.url}</p>
+      <p>likes: {blog.likes} <button>like</button></p>
+      <p>added by: {blog.user === undefined ?
+        'unknown' :
+        blog.user.name  
+        }
+    </p>
+
+      <button onClick={toggleVisibility}>hide</button>
+      
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisibility}>view</button>
+    </div>  
+  )
+}
 export default Blog
