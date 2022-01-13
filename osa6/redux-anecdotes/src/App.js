046e06443de6f6ee4d02a1cf1903c84e25c68869
Tuuli TG <tuuli.k.toivanen@gmail.com>
2022-01-13 14:45:34 +0200

    Tehtävät 6.3-6.8

---
+++ osa6/redux-anecdotes/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AnecdoteForm from './components/anecdoteForm'
+import AnecdoteList from './components/anecdoteList'
+
+const App = () => 
+    <div>
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+
+export default App
