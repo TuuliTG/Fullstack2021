c286c12fbf63b32fc5f7df6e4d6015883c4624f1
Tuuli TG <tuuli.k.toivanen@gmail.com>
2022-01-14 13:24:43 +0200

    Tehtävä 6.16

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -21,9 +21,12 @@ const reducer = (state = [], action) => {
 }
 
 export const createAnecdote = (anecdote) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    data: anecdote
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createNew(anecdote)
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      data: newAnecdote  
+    })
   }
 }
 
