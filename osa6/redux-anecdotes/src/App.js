a0a271abf1bae6ed30d74c4447c1c9d9a003fa2e
Tuuli TG <tuuli.k.toivanen@gmail.com>
2022-01-14 11:23:24 +0200

    Tehtävä 6.13

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import anecdoteService from './services/anecdotes'
+import { init } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/anecdoteForm'
 import AnecdoteList from './components/anecdoteList'
 import Notification from './components/Notification'
-const App = () => 
+
+const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(init(anecdotes)))
+  })
+  
+  return (
     <div>
       <Notification />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
-
+  )
+}
 export default App
